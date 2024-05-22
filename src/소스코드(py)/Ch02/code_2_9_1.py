0473a9fc11040de12e5537443ec8258bea70cf79
DongGyu PARK <dongupak@gmail.com>
2024-05-22 13:25:40 +0900

    Add files via upload

---
+++ src/소스코드(py)/Ch02/code_2_9_1.py
@@ -0,0 +1,9 @@
+#
+# 따라하며 배우는 파이썬과 데이터과학(생능출판사 2023 개정판)
+# 2.9 객체와 메소드 그리고 함수, 66쪽
+#
+
+import turtle
+t = turtle.Turtle()   #거북이 객체를 생성함
+t.shape("turtle")
+t.forward(100)
---
+++ src/소스코드(py)/Ch02/lab_2_3.py
@@ -0,0 +1,15 @@
+#
+# 따라하며 배우는 파이썬과 데이터과학(생능출판사 2023 개정판)
+# LAB 2-3 터틀 그래픽으로 피자그리기, 52쪽
+#
+
+import turtle
+
+t = turtle.Turtle()
+t.shape('turtle')
+
+radius = 100
+t.circle(radius)           # 반지름이 100인 원을 그린다.
+
+radius = 200
+t.circle(radius)           # 반지름이 200인 원을 그린다.
